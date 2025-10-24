1e7689343925ab6fcf6040c77c7af532d69d01dd
Vikas Reddy <kathulavikasr@gmail.com>
2025-10-24 17:10:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of Digits of a Number/sum-of-digits-of-a-number.java
@@ -0,0 +1,9 @@
+class Solution {
+    public static int sumOfDigits(int n) {
+        // Code here
+        if(n<=0){
+            return 0;
+        }
+        return n%10 + sumOfDigits(n/10);
+    }
+}
